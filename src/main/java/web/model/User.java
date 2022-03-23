5026e03e5aaedfd94f74bfe3d486d0f0720ee215
kolebakin <93770725+kolebakin@users.noreply.github.com>
2022-03-23 12:50:54 +0300

    Initial commit

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,67 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "position")
+    private String position;
+
+    @Column(name = "email")
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String position, String email) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User ID=" + id + ", name=" + name + ", position=" + position + ", email=" + email;
+    }
+}
